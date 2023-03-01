70dea6a06d51921d283828118bd4b0fd663da39a
Leonardo-villagran <leonardovillagran@yahoo.com>
2023-03-01 19:13:26 -0300

    Home text added

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -7,7 +7,7 @@ const Home = () => {
     <Container className="text-center">
       <h1 className="pt-5">
         Bienvenido a <span className="fw-bold">Happy Cake</span></h1>
-      <p>El lugar de los pasteles felices</p>
+      <p>El lugar de los pasteles felices.</p>
       <img className="imagen_pastel" src={image_pastel} alt={'foto'}/>
     </Container>
   );
